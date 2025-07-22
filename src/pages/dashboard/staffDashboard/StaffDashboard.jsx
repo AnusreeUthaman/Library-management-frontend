d5f3ce4e081158f37ee0259c26756e81f6de9625
AnusreeUthaman <anusreeuthaman02@gmail.com>
2025-07-22 11:40:05 +0530

    Staff dashboard

---
+++ src/pages/dashboard/staffDashboard/StaffDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StaffSidebar from "../../../sidebar/StaffSidebar";
+
+
+const StaffDashboard = () => (
+  <>
+    <StaffSidebar />
+     <h2 className='text-center text-3xl font-bold font-serif text-blue-950 '>Staff Dashboard</h2>
+     
+  </>
+);
+export default StaffDashboard;
